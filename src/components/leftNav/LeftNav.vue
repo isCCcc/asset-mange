7ca0d712c0ad20f7d53dec84c03b9cdfd0bcdf98
blanche <1935158902@qq.com>
2022-10-17 21:31:15 +0800

    完成框架得基本搭建

---
+++ src/components/leftNav/LeftNav.vue
@@ -0,0 +1,88 @@
+<template>
+  <el-menu class="el-menu-vertical-demo" background-color="#334157" text-color="#fff" active-text-color="#ffd04b">
+    <!--        <el-menu :default-openeds="['1', '3']">-->
+    <div class="logobox">
+      <img class="logoimg" src="../../assets/img/logo.png" alt="">
+    </div>
+    <el-submenu index="1">
+      <template slot="title"><i class="el-icon-message"></i>资产入库</template>
+      <el-menu-item-group>
+        <el-menu-item index="1-1">资产展示</el-menu-item>
+        <el-menu-item index="1-2">资产核实</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+
+    <el-submenu index="2">
+      <template slot="title"><i class="el-icon-menu"></i>资产出归</template>
+      <el-menu-item-group>
+        <el-menu-item index="2-1">出库</el-menu-item>
+        <el-menu-item index="2-2">归还</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+
+    <el-submenu index="3">
+      <template slot="title"><i class="el-icon-setting"></i>产品维修</template>
+      <el-menu-item-group>
+        <el-menu-item index="3-1">产品维修</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+
+    <el-submenu index="4">
+      <template slot="title"><i class="el-icon-setting"></i>资产报废</template>
+      <el-menu-item-group>
+        <el-menu-item index="3-1">报废记录</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+
+    <el-submenu index="5">
+      <template slot="title"><i class="el-icon-setting"></i>资产盘点</template>
+      <el-menu-item-group>
+        <el-menu-item index="3-1">资产盘点</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: "leftNav"
+}
+</script>
+
+<style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 240px;
+  height: 100vh;
+  overflow-y:hidden;
+}
+
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  border: none;
+  text-align: left;
+}
+
+.el-menu-item-group__title {
+  padding: 0px;
+}
+
+.el-menu-bg {
+  background-color: #1f2d3d !important;
+}
+
+.el-menu {
+  border: none;
+}
+
+.logobox {
+  height: 40px;
+  line-height: 40px;
+  color: #9d9d9d;
+  font-size: 20px;
+  text-align: center;
+  padding: 20px 0px;
+}
+
+.logoimg {
+  height: 40px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,80 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <!-- 侧边导航栏 -->
+  <el-container class="index-con" >
+    <el-aside style="width: 240px">
+      <LeftNav></LeftNav>
+    </el-aside>
+
+    <!--  头部导航栏  -->
+    <el-container class="main-con">
+      <el-header class="index-header" style="padding: 0">
+        <TopNav></TopNav>
+      </el-header>
+
+      <!--   内容主体   -->
+      <el-main clss="index-main">
+        hihi
+<!--        <el-table :data="tableData">-->
+<!--          <el-table-column prop="date" label="日期" width="140">-->
+<!--          </el-table-column>-->
+<!--          <el-table-column prop="name" label="姓名" width="120">-->
+<!--          </el-table-column>-->
+<!--          <el-table-column prop="address" label="地址">-->
+<!--          </el-table-column>-->
+<!--        </el-table>-->
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import LeftNav from "@/components/leftNav/LeftNav.vue";
+import TopNav from "@/components/topNav/TopNav.vue";
 
 export default {
-  name: 'HomeView',
+  data() {
+    const item = {
+      date: '2016-05-02',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄'
+    };
+
+    return {
+      tableData: Array(20).fill(item)
+    }
+  },
   components: {
-    HelloWorld
-  }
-}
+    TopNav,
+    LeftNav
+  },
+  // components: {left-nav}
+
+};
 </script>
+
+<style>
+.index-con {
+  height: 100%;
+  width: 100%;
+  box-sizing: border-box;
+}
+
+.aside {
+  width: 64px !important;
+  height: 100%;
+  background-color: #334157;
+  margin: 0px;
+}
+.asideshow {
+  width: 240px !important;
+  height: 100%;
+  background-color: #334157;
+  margin: 0px;
+}
+.index-header,
+.index-main {
+  padding: 0px;
+  border-left: 2px solid #333;
+  width: 100%;
+}
+</style>
